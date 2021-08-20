c5696c537562691eeedca64353b9f2a1d7622cc4
dablazizwe <56756546+dablazizwe@users.noreply.github.com>
2021-08-20 15:30:13 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
-		Maze large_file = new Maze("./resources/test.txt");
+		Maze large_file = new Maze("./resources/large_file.txt");
 
 		AStarPathFinder a2 = new AStarPathFinder(large_file, true);
 
